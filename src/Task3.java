86b1c9f4ffe41007445e6eb53af0e51f6793d22e
Gary Trakhman <gary.trakhman@gmail.com>
2013-03-24 20:59:46 -0400

    fibonacci

---
+++ src/Task3.java
@@ -0,0 +1,19 @@
+
+/**
+ * 1. Fibonacci.  A Fibonacci number is one of the set 1 1 2 3 5 8 ...
+ *    where the next is just the sum of the last two.  Create a recursive
+ *    function that implements fibonacci and returns the nth element.
+ *    (Hint: you have to handle the base cases or it'll blow up, that
+ *    requires an 'if'.)
+ */
+public class Task3{
+
+    public static int fib(int n){
+        // return fib(?) + fib(?);
+        return 0;
+    }
+
+    public static void main(String[] args){
+        System.out.println(fib(5));
+    }
+}
---
+++ src/Task4.java
@@ -0,0 +1,24 @@
+/**
+ * 1. Fibonacci.  A Fibonacci number is one of the set 1 1 2 3 5 8 ...
+ *    where the next is just the sum of the last two.  Create an iterative
+ *    function that implements fibonacci and returns the nth element.
+ *    (Hint: we know the first two values, from there we can find the
+ *    next, from there we can find the next, ...)
+ * 2. Notice that it's equivalent to the recursive.
+ * 3. Notice that there's no stackoverflow error here on large
+ *    numbers.  If you haven't tried a large number on the recursive
+ *    implementation, try it now.
+ */
+public class Task4{
+
+    public static int fib(int n){
+        int a = 1;
+        int b = 1;
+        // ...
+        return 0;
+    }
+
+    public static void main(String[] args){
+        System.out.println(fib(5));
+    }
+}
