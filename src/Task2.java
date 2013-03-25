c3243781ef18a5fe1b925c9db8e62f78afa34d0c
Gary Trakhman <gary.trakhman@gmail.com>
2013-03-24 21:24:05 -0400

    docs

--- src/Task2.java
+++ src/Task2.java
@@ -1,7 +1,7 @@
 
 /**
  * 1. Modify 'count' to print all the numbers from one to 10.  Use a loop.
- * 2. Modify 'count' to take an int parameter that determines that is
+ * 2. Modify 'count' to take an int parameter that determines what is
  *    the number to count to.  Modify the main to count to a different number.
  * 3. What happens when we pass in a negative number?   
  */
